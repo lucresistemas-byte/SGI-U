1eef17e75168df7028b84abfcb91986b91414809
lucioamarilla_ubuntu <lucioamarilla69@gmail.com>
2026-04-15 15:25:06 -0300

    feat: implementa clase base para auditoría de registros

---
+++ sgiu/src/main/java/com/sgiu_group/sgiu/models/base/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.sgiu_group.sgiu.models.base;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Identifiable, Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
