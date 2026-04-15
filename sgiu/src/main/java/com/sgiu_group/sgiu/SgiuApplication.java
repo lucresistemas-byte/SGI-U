fa966f88406639615a42c138a999296593df569e
lucioamarilla_ubuntu <lucioamarilla69@gmail.com>
2026-04-15 12:31:44 -0300

    feat: inicializa estructura base del proyecto con Maven y Spring Boot

---
+++ sgiu/src/main/java/com/sgiu_group/sgiu/SgiuApplication.java
@@ -0,0 +1,13 @@
+package com.sgiu_group.sgiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgiuApplication.class, args);
+	}
+
+}
---
+++ sgiu/src/test/java/com/sgiu_group/sgiu/SgiuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgiu_group.sgiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
