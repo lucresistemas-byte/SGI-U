c480b2b3c0818491476dcf9f533d71e88cfe29f7
lucioamarilla_ubuntu <lucioamarilla69@gmail.com>
2026-04-15 15:41:28 -0300

    feat: integra repositorios para acceso a datos mediante JPA

---
+++ sgiu/src/main/java/com/sgiu_group/sgiu/repositories/LineaVentaRepository.java
@@ -0,0 +1,8 @@
+package com.sgiu_group.repositories;
+
+
+import com.sgiu_group.sgiu.models.entities.LineaVenta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LineaVentaRepository extends JpaRepository<LineaVenta, Long> {
+}
---
+++ sgiu/src/main/java/com/sgiu_group/sgiu/repositories/MovFinancieroRepository.java
@@ -0,0 +1,7 @@
+package com.sgiu_group.repositories;
+
+import com.sgiu_group.sgiu.models.entities.MovFinanciero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MovFinancieroRepository extends JpaRepository<MovFinanciero, Long> {
+}
