94bad17f4ecd1ef312032e9e677a57542a9617f6
lucioamarilla_ubuntu <lucioamarilla69@gmail.com>
2026-04-18 01:48:41 -0300

    fix: corrige nombre de producto

--- backend/sgiu/src/main/resources/data.sql
+++ backend/sgiu/src/main/resources/data.sql
@@ -3,7 +3,7 @@
 -- =========================
 -- Se agrega la columna 'nombre' (CU02) y 'activo' (RN08)
 INSERT IGNORE INTO esp_productos (id, codigo, nombre, precio_unitario, activo, created_at, updated_at) VALUES
-(1, 'PROD-001', 'Arroz Integral 1kg"', 100.00, 1, NOW(), NOW()),
+(1, 'PROD-001', 'Arroz Integral 1kg', 100.00, 1, NOW(), NOW()),
 (2, 'PROD-002', 'Leche Entera 1L', 250.50, 1, NOW(), NOW()),
 (3, 'PROD-003', 'Cerveza Brahma', 75.25, 1, NOW(), NOW()),
 (4, 'PROD-004', 'Carbon Vegetal 5kg', 320.00, 1, NOW(), NOW()),
