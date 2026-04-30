59a477452fcfdd369a703177c4d78703db37f0df
Luciano Chesani <lolochesani@gmail.com>
2026-04-30 09:54:02 -0300

    FIX: Habilitar CORS en ProductoController para permitir integración con Frontend

--- backend/sgiu/src/main/java/com/sgiu_group/sgiu/controllers/ProductoController.java
+++ backend/sgiu/src/main/java/com/sgiu_group/sgiu/controllers/ProductoController.java
@@ -7,10 +7,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/productos")
 @RequiredArgsConstructor
 public class ProductoController {
--- backend/sgiu/src/main/java/com/sgiu_group/sgiu/controllers/VentaController.java
+++ backend/sgiu/src/main/java/com/sgiu_group/sgiu/controllers/VentaController.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/ventas")
 public class VentaController {
 
