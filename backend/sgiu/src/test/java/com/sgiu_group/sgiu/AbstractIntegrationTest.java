9ebb94db1e0b60e2665b6de4ebe31d38d4840afa
lucioamarilla <lucioamarilla69@gmail.com>
2026-09-21 14:25:20 -0300

    test(back): preparar infraestructura de tests con MockMvc y factory de datos

--- backend/sgiu/src/test/java/com/sgiu_group/sgiu/AbstractIntegrationTest.java
+++ backend/sgiu/src/test/java/com/sgiu_group/sgiu/AbstractIntegrationTest.java
@@ -1,17 +1,30 @@
 package com.sgiu_group.sgiu;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
+@Transactional
 public abstract class AbstractIntegrationTest {
 
     @Autowired
     protected JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    protected MockMvc mockMvc;
+
+    /**
+     * Como las clases de integración son @Transactional, cada test revierte sus
+     * escrituras al final. Este método se conserva por compatibilidad con tests
+     * previos que dependían de la limpieza manual.
+     */
     protected void limpiarBaseDatos() {
         jdbcTemplate.execute("DELETE FROM movimientos_financieros");
         jdbcTemplate.execute("DELETE FROM articulos_stock");
